6868598744cda29eebc2596a05768db0614cf7dc
Ali Haroon <fa23-bcs-243@students.cuisahiwal.edu.pk>
2026-05-08 23:17:53 +0500

    Building Personal Portfolio: Fixes Bug and typos

---
+++ script.js
@@ -0,0 +1,31 @@
+const textElement = document.querySelector('.typewriter')
+const words = ["Full Stack Developer", "BSCS Student", "Node.js Enthusiast"];
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function type(){
+    const currentWord = words[wordIndex];
+
+    if(isDeleting) {
+        textElement.textContent = currentWord.substring(0, charIndex - 1);
+        charIndex--
+    } else {
+        textElement.textContent = currentWord.substring(0, charIndex + 1);
+        charIndex++
+    }
+
+    if (!isDeleting && charIndex == currentWord.length) {
+        isDeleting = true;
+        setTimeout(type,2000);
+    } else if (isDeleting && charIndex === 0) {
+        isDeleting = false;
+        wordIndex = (wordIndex + 1) % words.length;
+        setTimeout(type, 500);
+    } else {
+        setTimeout(type, isDeleting ? 50 : 150);
+    }
+
+}
+
+type();
