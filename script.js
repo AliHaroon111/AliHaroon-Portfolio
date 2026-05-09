a076e76e3efcb5ef60bd88e0cc1e3661ed6234f9
Ali Haroon <fa23-bcs-243@students.cuisahiwal.edu.pk>
2026-05-09 08:57:45 +0500

    Building Personal Portfolio: Fixes Some issue

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const textElement = document.querySelector('.typewriter')
+let sections = document.querySelectorAll('section')
+let navLinks = document.querySelectorAll('header nav a');
 const words = ["Full Stack Developer", "BSCS Student", "Node.js Enthusiast"];
 let wordIndex = 0;
 let charIndex = 0;
@@ -26,6 +28,24 @@ function type(){
         setTimeout(type, isDeleting ? 10 : 30);
     }
 
+}
+
+// Navbar Scroll
+window.onscroll = ()=>{
+    sections.forEach(section => {
+        let top =window.scrollY;
+        let offset = section.offsetTop - 150;
+        let height = section.offsetHeight;
+        let id = section.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+            });
+        }
+        
+    });
 }
 
 type();
